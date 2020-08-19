eea985892d57ddc11b12cd5b498c9393c7c80820
Lyndsay Crespo <lyndsaycrespo@gmail.com>
2020-08-19 19:36:45 -0400

    my second commit

--- week02/02.02.js
+++ week02/02.02.js
@@ -11,7 +11,21 @@
 /*
 
 git init in the cmd prompt in the folder you want to upload to git
-git add. (adds everything in the current directory to a staging area - you can also type an individual file name)
+git add . (adds everything in the current directory to a staging area - you can also type an individual file name)
 git status 
+git commit -m "my first commit"
+
+if you modify a file after committing, you need to add it to the staging area again and then commit it again
+
+go to github, log in, and create a new repository. once you fill in the info, push an existing repository from the cmd line. it will 
+give you a cmd line statement that looks like this:
+git remote add origin https://github.com/lyndsaycrespo/jsLectureNotesWks1and2.git (enter it into the cmd line)
+it will give you another line as well that looks like this:
+git push -u origin master (enter this into the cmd line as well)
+
+let's make a change
+check the status with git status
+then git commit -m
+then git push origin master
 
 */
